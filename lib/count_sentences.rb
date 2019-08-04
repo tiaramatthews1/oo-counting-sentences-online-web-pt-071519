38046f99e20f0767d1624d283bf86bc4c56425e7
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-08-04 19:20:00 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -13,8 +13,10 @@ class String
   end
 
   def count_sentences
-    self.split(/[.!?]/).rject{|x| x.empty?}.size 
+    self.split(/[.!?]/).reject{|x| x.empty?}.size 
 end
 
-
+def count_sentences_words
+  self.split(/[.!?]/).map{|x|!(x.match(/\w+/).nil?)}.reject{|x| x == false} .size
+end
 end
