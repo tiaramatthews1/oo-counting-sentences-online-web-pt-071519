2a0d92bc9187fe0faec02a74b9e44ddcf9967cf7
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-08-04 19:20:14 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,22 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    self.split(/[.!?]/).reject{|x| x.empty?}.size 
+end
 
+def count_sentences_words
+  self.split(/[.!?]/).map{|x|!(x.match(/\w+/).nil?)}.reject{|x| x == false} .size
   end
 end
